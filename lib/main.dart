edec42e473cf98b7ba4cbd22f4cbc287ce5e34d7
Sam Quintero <stqu224@uky.edu>
2023-07-14 10:22:31 -0400

    I might have fixed it so it should work and do API thingies now

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,109 @@
-import 'dart:async';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audio Recorder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  FlutterSoundRecorder? _audioRecorder;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterSoundRecorder? _recorder;
   bool _isRecording = false;
-  String _recordingPath = '';
 
   @override
   void initState() {
     super.initState();
-    _initializeAudioRecorder();
-  }
-
-  void _initializeAudioRecorder() async {
-    _audioRecorder = FlutterSoundRecorder();
-
-    await _audioRecorder!.openAudioSession();
-    await _audioRecorder!.setSubscriptionDuration(Duration(milliseconds: 10));
-  }
-
-  void _startRecording() async {
-    final appDir = await getApplicationDocumentsDirectory();
-    final recordingName = DateTime.now().toString() + '.wav';
-    final recordingPath = '${appDir.path}/$recordingName';
-
-    setState(() {
-      _isRecording = true;
-      _recordingPath = recordingPath;
-    });
-
-    await _audioRecorder!.startRecorder(toFile: recordingPath);
-  }
-
-  void _stopRecording() async {
-    setState(() {
-      _isRecording = false;
-    });
-
-    final recording = File(_recordingPath);
-    final stream = http.ByteStream(Stream.castFrom(recording.openRead()));
-    final request = http.MultipartRequest('POST', Uri.parse('YOUR_API_URL'));
-
-    final multipartFile = http.MultipartFile(
-        'audio', stream, recording.lengthSync(),
-        filename: _recordingPath);
-    request.files.add(multipartFile);
-
-    final response = await request.send();
-    if (response.statusCode == 200) {
-      print('Audio uploaded successfully');
-    } else {
-      print('Failed to upload audio. Status code: ${response.statusCode}');
-    }
-
-    await _audioRecorder!.stopRecorder();
+    _recorder = FlutterSoundRecorder();
+    _recorder!.openAudioSession();
   }
 
   @override
   void dispose() {
-    _audioRecorder!.closeAudioSession();
-    _audioRecorder = null;
+    _recorder!.closeAudioSession();
     super.dispose();
+  }
+
+  Future<void> _startRecording() async {
+    final tempDir = await getTemporaryDirectory();
+    final audioPath = '${tempDir.path}/recording.wav';
+    await _recorder!.startRecorder(toFile: audioPath);
+
+    setState(() {
+      _isRecording = true;
+    });
+  }
+
+  Future<void> _stopRecording() async {
+    final result = await _recorder!.stopRecorder();
+
+    if (result != null) {
+      final audioPath = result;
+
+      // Make POST request to API
+      final response = await http.post(
+        Uri.parse('API_URL'),
+        headers: {
+          HttpHeaders.contentTypeHeader: 'audio/wav',
+        },
+        body: File(audioPath).readAsBytesSync(),
+      );
+
+      if (response.statusCode == 200) {
+        // Successfully uploaded audio
+        print('Audio uploaded successfully');
+      } else {
+        // Failed to upload audio
+        print('Failed to upload audio');
+      }
+    }
+
+    setState(() {
+      _isRecording = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Audio Recorder'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_isRecording) Text('Recording...') else Text('Not Recording'),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _isRecording ? _stopRecording : _startRecording,
-                child:
-                    Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Audio Recorder'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Audio Recording',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            IconButton(
+              iconSize: 80,
+              onPressed: _isRecording ? _stopRecording : _startRecording,
+              icon: Icon(
+                _isRecording ? Icons.stop : Icons.mic,
+                color: _isRecording ? Colors.red : Colors.blue,
               ),
-              SizedBox(height: 16.0),
-              Text('Recording Path:'),
-              Text(_recordingPath),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
