9c44cb91f4c5957629cb0a66c13c56f7c05e3e8e
Sam Quintero <stqu224@uky.edu>
2023-07-28 20:47:39 -0400

    hopefully works now, at least i hope so. Added comments and such

--- lib/main.dart
+++ lib/main.dart
@@ -26,45 +26,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  FlutterSoundRecorder? _recorder;
-  bool _isRecording = false;
+  FlutterSoundRecorder?
+      _recorder; // FlutterSoundRecorder object to manage audio recording.
+  bool _isRecording = false; // Flag to keep track of recording status.
 
   @override
   void initState() {
     super.initState();
-    _recorder = FlutterSoundRecorder();
-    _recorder!.openAudioSession();
+    _recorder =
+        FlutterSoundRecorder(); // Initializing the FlutterSoundRecorder.
+    _recorder!.openAudioSession(); // Opening the audio session.
   }
 
   @override
   void dispose() {
-    _recorder!.closeAudioSession();
+    _recorder!
+        .closeAudioSession(); // Closing the audio session when the widget is disposed.
     super.dispose();
   }
 
   Future<void> _startRecording() async {
-    final tempDir = await getTemporaryDirectory();
-    final audioPath = '${tempDir.path}/recording.wav';
-    await _recorder!.startRecorder(toFile: audioPath);
+    final tempDir =
+        await getTemporaryDirectory(); // Getting the temporary directory path.
+    final audioPath =
+        '${tempDir.path}/recording.wav'; // Creating the path for the audio file.
+    await _recorder!
+        .startRecorder(toFile: audioPath); // Starting the audio recording.
 
     setState(() {
-      _isRecording = true;
+      _isRecording = true; // Updating the recording status to true.
     });
   }
 
   Future<void> _stopRecording() async {
-    final result = await _recorder!.stopRecorder();
+    final result =
+        await _recorder!.stopRecorder(); // Stopping the audio recording.
 
     if (result != null && result.isNotEmpty) {
-      final audioPath = result;
+      final audioPath = result; // Getting the path of the recorded audio.
 
       // Make POST request to API
       final response = await http.post(
-        Uri.parse('http://10.47.166.34:5055/api/flutter'),
+        Uri.parse(
+            'http://10.47.166.34:5000/api/flutter'), // API endpoint to send the audio data.
         headers: {
-          HttpHeaders.contentTypeHeader: 'audio/wav',
+          HttpHeaders.contentTypeHeader:
+              'audio/wav', // Setting the content type for the request.
         },
-        body: File(audioPath).readAsBytesSync(),
+        body: File(audioPath)
+            .readAsBytesSync(), // Reading the audio file as bytes and sending it in the request body.
       );
 
       if (response.statusCode == 200) {
@@ -79,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     setState(() {
-      _isRecording = false;
+      _isRecording = false; // Updating the recording status to false.
     });
   }
 
@@ -100,10 +110,18 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(height: 20),
             IconButton(
               iconSize: 80,
-              onPressed: _isRecording ? _stopRecording : _startRecording,
+              onPressed: _isRecording
+                  ? _stopRecording
+                  : _startRecording, // Handling the button press for recording and stopping.
               icon: Icon(
-                _isRecording ? Icons.stop : Icons.mic,
-                color: _isRecording ? Colors.red : Colors.blue,
+                _isRecording
+                    ? Icons.stop
+                    : Icons
+                        .mic, // Displaying different icons based on recording status.
+                color: _isRecording
+                    ? Colors.red
+                    : Colors
+                        .blue, // Changing the icon color based on recording status.
               ),
             ),
           ],
